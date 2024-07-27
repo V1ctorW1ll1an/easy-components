9091f2d5063a1befcbad4f83ea9a0f184b50e87c
victor <victor.willian@solucionarti.com.br>
2024-07-27 09:45:04 -0300

    feat: :sparkles: initial commit of Tailwind CSS build script
    - Implemented a Bash script to automate the Tailwind CSS build process.
    - Added support for individual file minification using the 'minify' command, monitoring file changes with the 'watch' command, and batch minification for all components and pages with 'minify:all'.
    - The script determines the relevant file paths for components and pages, ensuring CSS is generated in the correct directory.
    - Improved error handling, providing informative messages when files are not found.
    - Updated script usage documentation and examples for better guidance.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,86 +1,86 @@
 #!/bin/bash
 
-# Diretórios dos componentes
-COMPONENTS=( "header" "swiper" )
-
-# Caminho base para os componentes
-BASE_PATH="./src/components"
-
-# Função para gerar o CSS
-generate_css() {
-  COMPONENT=$1
-  INPUT_HTML="$BASE_PATH/$COMPONENT/${COMPONENT}.html"
-  INPUT_JS="$BASE_PATH/$COMPONENT/${COMPONENT}.js"
-  OUTPUT_FILE="$BASE_PATH/$COMPONENT/output.css"
-
-  echo "Gerando CSS para o componente $COMPONENT..."
-  npx tailwindcss -i ./src/input.css -o $OUTPUT_FILE --content $INPUT_HTML,$INPUT_JS
-}
-
-# Função para assistir mudanças em um componente
-watch_component() {
-  COMPONENT=$1
-  INPUT_HTML="$BASE_PATH/$COMPONENT/${COMPONENT}.html"
-  INPUT_JS="$BASE_PATH/$COMPONENT/${COMPONENT}.js"
-
-  echo "Assistindo mudanças no componente $COMPONENT..."
-  npx tailwindcss -i ./src/input.css -o $BASE_PATH/$COMPONENT/output.css --content $INPUT_HTML,$INPUT_JS --watch
-}
-
-# Função para mostrar mensagem de ajuda
-show_help() {
-  echo "Uso: $0 {build|watch} [componente]"
-  echo
-  echo "Opções:"
-  echo "  build         Gera o CSS para um ou todos os componentes."
-  echo "  watch         Monitora mudanças nos arquivos de um componente e gera o CSS automaticamente."
-  echo
-  echo "Componentes disponíveis: ${COMPONENTS[@]}"
-  echo
-  echo "Exemplos:"
-  echo "  $0 build           Gera o CSS para todos os componentes."
-  echo "  $0 build header    Gera o CSS apenas para o componente 'header'."
-  echo "  $0 watch swiper    Monitora mudanças no componente 'swiper' e gera o CSS automaticamente."
-}
-
-# Argumentos do script
+ROOT_PATH="./src"
+INPUT_PATH="$ROOT_PATH/input.css"
+PATHS_TO_STAY_MONITORED=(
+    "$ROOT_PATH/components"
+    # "$ROOT_PATH/pages"
+)
+EXTENSIONS=("html" "js")
+EXTENSIONS_LIST=$(printf ",%s" "${EXTENSIONS[@]}")
+EXTENSIONS_LIST=${EXTENSIONS_LIST:1}  # Remove the leading comma
 ACTION=$1
-COMPONENT_NAME=$2
+FILE_NAME=$2
 
-# Verificação dos argumentos
-if [[ -z "$ACTION" ]]; then
-  echo "Erro: Ação não especificada."
-  show_help
-  exit 1
-fi
+show_help() {
+  echo "Usage: $0 {minify|watch|minify:all} [component or page]"
+  echo
+  echo "Options:"
+  echo "  minify         Generates CSS for a specific component or page."
+  echo "  watch         Monitors changes in the files of a component or page and generates CSS automatically."
+  echo "  minify:all     Generates CSS for all components and pages in their respective directories."
+  echo
+  echo "Examples:"
+  echo " pnpm run tailwind:minify header         Generates CSS for the 'header' component."
+  echo " pnpm run tailwind:minify home           Generates CSS for the 'home' page."
+  echo " pnpm run tailwind:watch swiper         Monitors changes in the 'swiper' component and generates CSS automatically."
+  echo " pnpm run tailwind:minify:all           Generates CSS for all components and pages."
+}
 
-if [[ "$ACTION" == "build" || "$ACTION" == "watch" ]]; then
-  if [[ -n "$COMPONENT_NAME" ]]; then
-    if [[ " ${COMPONENTS[@]} " =~ " $COMPONENT_NAME " ]]; then
-      if [[ "$ACTION" == "build" ]]; then
-        generate_css $COMPONENT_NAME
-      elif [[ "$ACTION" == "watch" ]]; then
-        watch_component $COMPONENT_NAME
-      fi
+find_file() {
+    local filename="$1"
+    local found_file=""
+
+    for path in "${PATHS_TO_STAY_MONITORED[@]}"; do
+        for ext in "${EXTENSIONS[@]}"; do
+            # /dev/bull silence the default error msg
+            found=$(find "$path" -type f -name "$filename.$ext" 2>/dev/null)
+            if [[ -n "$found" ]]; then
+                found_file="$found"
+                break 2
+            fi
+        done
+    done
+
+    if [[ -z "$found_file" ]]; then
+        echo "Error, file:$FILE_NAME not found!" >&2
+        return 1
     else
-      echo "Erro: Componente '$COMPONENT_NAME' não encontrado."
-      show_help
-      exit 1
+        local dir_path="${found_file%/*}"
+        echo "$dir_path"
     fi
-  else
-    # Se nenhum componente específico for fornecido, realiza a ação para todos os componentes
-    if [[ "$ACTION" == "build" ]]; then
-      for COMPONENT in "${COMPONENTS[@]}"; do
-        generate_css $COMPONENT
-      done
-    else
-      echo "Erro: Ação 'watch' requer especificação de componente."
-      show_help
-      exit 1
-    fi
+}
+
+do_action() {
+  FILE_PATH=$(find_file "$FILE_NAME" 2>/dev/null)
+  if [ $? -ne 0 ]; then
+    # redirect standard error output to the standard error descriptor
+    echo "Error, file:$FILE_NAME not found!" >&2
+    exit 1
   fi
-else
-  echo "Erro: Ação inválida '$ACTION'."
-  show_help
-  exit 1
-fi
+
+  OUTPUT_FILE="$FILE_PATH/$FILE_NAME.css"
+
+  echo "$ACTION all {${EXTENSIONS_LIST}} files in: $FILE_PATH/"
+  npx tailwindcss -i "$INPUT_PATH" -o "$OUTPUT_FILE" --content "$FILE_PATH/*.{${EXTENSIONS_LIST}}" --$ACTION
+}
+
+minify_all() {
+    for path in "${PATHS_TO_STAY_MONITORED[@]}"; do
+        for dir in "$path"/*/; do
+            dir_name=$(basename "$dir")
+            OUTPUT_FILE="$dir/${dir_name}.css"
+            echo "Minifying all files in: $dir"
+            npx tailwindcss -i "$INPUT_PATH" -o "$OUTPUT_FILE" --content "$dir/*.{${EXTENSIONS_LIST}}" --minify
+        done
+    done
+}
+
+
+
+case $ACTION in
+"minify") do_action;;
+"watch") do_action;;
+"minify:all") minify_all;;
+*) show_help
+esac
